f26293c75edf81d8b3bd381f21971894f91ede7c
DavidTG7 <davidthekoa@gmail.com>
2022-08-31 21:40:22 -0500

    Create bars, get data from Json  file

--- src/pages/expenses_chart/ExpensesChart.styles.js
+++ src/pages/expenses_chart/ExpensesChart.styles.js
@@ -1,3 +1,91 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const ExpensesMain = styled.main``;
+export const ExpensesMain = styled.main`
+  background: hsl(27, 66%, 92%);
+  display: grid;
+  place-items: center;
+  padding: 2rem 0;
+`;
+
+export const HeaderBox = styled.div`
+  box-sizing: border-box;
+  color: hsl(33, 100%, 98%);
+  background: hsl(10, 79%, 65%);
+  border-radius: 10px;
+  padding: 2rem;
+  width: 330px;
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TextBox = styled.div`
+  line-height: 1.5;
+`;
+
+export const HeaderTitle = styled.h2`
+  font-family: "DM Sans";
+  font-size: 1.5rem;
+  font-weight: 400;
+`;
+
+export const HeaderPrice = styled.p`
+  font-weight: 600;
+  font-family: "DM Sans";
+  font-size: 2.5rem;
+`;
+
+export const CirclesBox = styled.div`
+  display: flex;
+  position: relative;
+`;
+
+export const LeftCircle = styled.div`
+  position: absolute;
+  box-sizing: border-box;
+  border-radius: 50%;
+  width: 4rem;
+  height: 4rem;
+  border: 2px solid hsl(33, 100%, 98%);
+  margin-left: -50%;
+`;
+
+export const RightCircle = styled.div`
+  border-radius: 50%;
+  background: hsl(25, 47%, 15%);
+  width: 4rem;
+  height: 4rem;
+`;
+export const CardBox = styled.div`
+  box-sizing: border-box;
+  color: hsl(25, 47%, 15%);
+  padding: 1rem;
+  width: 330px;
+  margin: 1rem;
+  border-radius: 10px;
+  background: hsl(33, 100%, 98%);
+`;
+
+export const ColumnsBox = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-around;
+`;
+
+export const Column = styled.div`
+  width: 3.4rem;
+  border-radius: 4px;
+  height: ${({ value }) => `calc(${value} * 3px)`};
+  background: ${({ color }) =>
+    color === "r" ? "hsl(10, 79%, 65%)" : "hsl(186, 34%, 60%)"};
+`;
+
+export const DayBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+`;
+export const Day = styled.p``;
--- src/pages/expenses_chart/ExpensesChart.tsx
+++ src/pages/expenses_chart/ExpensesChart.tsx
@@ -1,9 +1,37 @@
-import { ExpensesMain } from "./ExpensesChart.styles";
+import { CardBox, CirclesBox, Column, ColumnsBox, Day, DayBox, ExpensesMain, HeaderBox, HeaderPrice, HeaderTitle, LeftCircle, RightCircle, TextBox } from "./ExpensesChart.styles";
+import data from './data.json'
 
 export const ExpensesChart = () => {
-  return(
+  const allData = Object.values(data);
+
+
+  const allColumns = allData.map(item => {
+    const color = item.day === 'wed' ? '' : 'r';
+    return (
+      <DayBox>
+        <Column color={color} value={item.amount} />
+        <Day>{item.day}</Day>
+      </DayBox>
+    );
+  });
+
+  return (
     <ExpensesMain>
-      Expenses!
+      <HeaderBox>
+        <TextBox>
+          <HeaderTitle>My balance</HeaderTitle>
+          <HeaderPrice>$921.48</HeaderPrice>
+        </TextBox>
+        <CirclesBox>
+          <LeftCircle />
+          <RightCircle />
+        </CirclesBox>
+      </HeaderBox>
+      <CardBox>
+        <ColumnsBox>
+          {allColumns}
+        </ColumnsBox>
+      </CardBox>
     </ExpensesMain>
   );
 };
