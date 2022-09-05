b9df67b89dc2de0b2aa34466e15520853f74ed44
DavidTG7 <davidthekoa@gmail.com>
2022-09-04 21:38:32 -0500

    Add new path for Interactive card

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Main } from "./components/Main/Main";
 import { ExpensesChart } from "./pages/expenses_chart/ExpensesChart";
+import { InteractiveCard } from "./pages/interactive_card/InteractiveCard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/expenses-chart' element={<ExpensesChart />} />
+        <Route path='/interactive-card' element={<InteractiveCard />} />
       </Routes>
       <Footer />
     </>
